56b4185715eff2066eee00e158fa02d368b447df
hyunsoo121 <ggom121@naver.com>
2024-12-20 13:26:56 +0900

    feat: userServiceImpl 구현

--- demo/demo/src/main/java/com/civilWar/demo/domain/user/UserServiceImpl.java
+++ demo/demo/src/main/java/com/civilWar/demo/domain/user/UserServiceImpl.java
@@ -1,30 +1,36 @@
 package com.civilWar.demo.domain.user;
 
 import com.civilWar.demo.domain.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
     @Override
     public void save(User user) {
-
+        userRepository.save(user);
     }
 
     @Override
     public User findById(Long id) {
-        return null;
+        return userRepository.findById(id).orElse(null);
     }
 
     @Override
     public User findByEmail(String email) {
-        return null;
+        return userRepository.findByEmail(email).orElse(null);
     }
 
     @Override
     public void deleteById(Long id) {
-
+        userRepository.deleteById(id);
     }
 
     @Override
     public void update(User user) {
-
+        userRepository.save(user);
     }
 }
